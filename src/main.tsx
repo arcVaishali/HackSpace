9767a8208629ff0a5cbf8932d2d92c9bfd4908ea
Suven-p <pandeysuven@gmail.com>
2023-07-22 20:57:02 +0545

    Add template for login and register pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import Login from './pages/login.tsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.tsx';
+import Login from './pages/login.tsx';
+import Register from './pages/register.tsx';
 
 const router = createBrowserRouter([
   {
@@ -13,7 +15,11 @@ const router = createBrowserRouter([
   {
     path: 'login',
     element: <Login />,
-  }
+  },
+  {
+    path: 'register',
+    element: <Register />,
+  },
 ]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import circularLogo from '../assets/HackSpaceLogo/CircularLogo/2.png';
 import { Link } from 'react-router-dom';
 
-const login = () => {
+const Register = () => {
     const onSubmit = (e) => {
         e.preventDefault();
     };
@@ -24,14 +24,14 @@ const login = () => {
                     placeholder='Password'
                 />
                 <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-3/4'>
-                    Login
+                    Signup
                 </button>
-                <Link to="/register" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-3/4 text-center'>
-                    Register
+                <Link to="/login" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-3/4 text-center'>
+                    Login
                 </Link>
             </form>
         </div>
     );
 };
 
-export default login;
+export default Register;
