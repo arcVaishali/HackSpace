66af3348c064c99723909d59265dc8c197a2099b
Suven-p <pandeysuven@gmail.com>
2023-07-23 13:54:48 +0545

    Fix merge errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import './App.css'
-<<<<<<< HEAD
-import MyWorkSpace from './pages/myworkspace'
-
-=======
+import './App.css';
 import Navbar from './components/Navbar';
 import MyWorkSpace from './pages/Workspace';
->>>>>>> 35c0e68c69ef19b7efc12a7c7f66e4bd12b235c2
-
 
 function App() {
-
-  return (
-<<<<<<< HEAD
-    <>
-    
-    <MyWorkSpace/>
-    </>
-    // <div className="text-3xl font-bold underline">
-    //    This is the home page.
-    // </div>
-  )
-=======
-      <>
-          <Navbar />
-      </>
-      // <div className="text-3xl font-bold underline">
-      //    This is the home page.
-      // </div>
-  );
->>>>>>> 35c0e68c69ef19b7efc12a7c7f66e4bd12b235c2
+    return (
+        <>
+            <Navbar />
+        </>
+        // <div className="text-3xl font-bold underline">
+        //    This is the home page.
+        // </div>
+    );
 }
 
-export default App
+export default App;
