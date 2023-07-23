39c8d229aff080bbd551a0df18fbac901681864f
vaishali <vaishalirdj14@gmail.com>
2023-07-23 11:19:47 +0530

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
+import MyWorkSpace from './pages/myworkspace'
 
 
 function App() {
 
   return (
+    <>
+    <MyWorkSpace/>
    
-
-    <div className="text-3xl font-bold underline">
-       This is the home page.
-    </div>
+    </>
+    // <div className="text-3xl font-bold underline">
+    //    This is the home page.
+    // </div>
   )
 }
 
